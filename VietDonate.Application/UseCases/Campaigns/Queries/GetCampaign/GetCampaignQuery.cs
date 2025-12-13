08f37c3857e047094cbe755b72d4cfc3f0003048
thanhdt <thanhdt@softdreams.vn>
2025-12-13 17:33:00 +0700

    Feature: crud-campaign

---
+++ VietDonate.Application/UseCases/Campaigns/Queries/GetCampaign/GetCampaignQuery.cs
@@ -0,0 +1,8 @@
+﻿using VietDonate.Application.Common.Mediator;
+using VietDonate.Application.Common.Result;
+using VietDonate.Domain.Model.Campaigns;
+
+namespace VietDonate.Application.UseCases.Campaigns.Queries.GetCampaign
+{
+    public record GetCampaignQuery(string CampaignId) : IQuery<Result<Campaign>>;
+}
---
+++ VietDonate.Application/UseCases/Campaigns/Queries/GetCampaign/GetCampaignQueryHandle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VietDonate.Application.Common.Interfaces.IRepository;
+using VietDonate.Application.Common.Mediator;
+using VietDonate.Application.Common.Result;
+using VietDonate.Domain.Model.Campaigns;
+
+namespace VietDonate.Application.UseCases.Campaigns.Queries.GetCampaign
+{
+    public class GetCampaignQueryHandle(ICampaignRepository campaignRepository) : IQueryHandler<GetCampaignQuery, Result<Campaign>>
+    {
+        public Task<Result<Campaign>> Handle(GetCampaignQuery query, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
